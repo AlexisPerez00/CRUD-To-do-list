702e6ac7d844347a101a4ab03bb9fd1806d8158b
Alexis-Perez <alexisperez00.cappi@gmail.com>
2021-08-12 21:28:00 -0500

    pulir detalles

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,23 +4,7 @@ import { useEffect, useState } from 'react'
     const TodoItem = ( { task, student, id, deleted, handleUpdate, isCompleted } ) => {
 
     const [style, setStyle] = useState('')  
-    // const [background_color, setBackgroundColor] = useState('') 
-    // const [lineThrough, setLineThrough] = useState('')
-    // const [fcolor, setFColor] = useState('')
 
-    // useEffect(() =>{
-    //     if(!isCompleted){
-    //         setColor('white')
-    //         setBackgroundColor('#2ea8df')
-    //         setLineThrough('none')
-    //         setFColor('black')
-    //     }else{
-    //         setColor('black')
-    //         setBackgroundColor('#71EFA3')
-    //         setLineThrough('line-through')
-    //         setFColor('green')
-    //     }
-    // }, [isCompleted])
     useEffect(() => {
         if(isCompleted) {
             setStyle("completed")
@@ -32,9 +16,7 @@ import { useEffect, useState } from 'react'
 
     return (
         <div className={style}>
-            {/* {isCompleted ? setStyle("completed")
-            : setStyle("incompleted")} */}
-            {/* <div className={style}> */}
+
                 
                 <p>Task: {task}</p>
                 <p>Student: {student}</p> 
@@ -43,7 +25,6 @@ import { useEffect, useState } from 'react'
                 <div className="buttons">
                     <button  className="delete" onClick={() => { deleted(id) } }></button>
                     <button  className="complete" onClick={() => { handleUpdate(id, {task, student, isCompleted}) }}></button>
-                {/* </div> */}
             
             </div>
              
